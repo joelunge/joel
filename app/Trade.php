68956d2073566afcc423e2571db7ec8822009303
Joel Unge <joel.unge@gmail.com>
2018-09-04 21:12:24 +0200

    Exclude new exchange trades

--- app/Trade.php
+++ app/Trade.php
@@ -12,6 +12,9 @@ class Trade extends Model
 		$query = $query->where('bitfinex_id', '!=', '216144904');
 		$query = $query->where('bitfinex_id', '!=', '216145205');
 		$query = $query->where('bitfinex_id', '!=', '216145209');
+		$query = $query->where('bitfinex_id', '!=', '289192249');
+		$query = $query->where('bitfinex_id', '!=', '289192252');
+		$query = $query->where('bitfinex_id', '!=', '289192265');
 
 		return $query;
 	}
