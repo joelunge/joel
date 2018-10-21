6e64502afa64b8213dfdc222847ee554cf8df8ad
Joel Unge <joel.unge@gmail.com>
2018-10-21 14:25:55 +0200

    Add history helper

---
+++ app/Libraries/History.php
@@ -0,0 +1,13 @@
+<?php
+
+class History
+{
+    public static function trades($ticker, $start, $end, $limit = 1000, $sort = -1)
+    {
+    	$requestUrl = sprintf('https://api.bitfinex.com/v2/trades/t%s/hist?limit=%s&start=%s&end=%s&sort=%s', $ticker, $limit, $start, $end, $sort);
+
+    	$content = file_get_contents($requestUrl);
+
+    	H::pr($content);
+    }
+}
