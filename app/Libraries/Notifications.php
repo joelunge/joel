61c1252995590610c861ea4a534fb91c15f88f03
Joel Unge <joel.unge@gmail.com>
2018-10-21 14:27:14 +0200

    Adjust slack notification

--- app/Libraries/Notifications.php
+++ app/Libraries/Notifications.php
@@ -4,7 +4,7 @@ class Notifications
 {
     public static function slack($coins)
     {
-    	$message = implode('\r\n', $coins) . 'https://www.tradingview.com/chart/xBDH50cD/';
+    	$message = '\r\n' . '===================' . '\r\n' . implode('\r\n', $coins) . '\r\n' . '===================' . '\r\n';
     	$webhook = 'https://hooks.slack.com/services/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';
 
     	exec(sprintf("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"%s\"}' %s", $message, $webhook));
