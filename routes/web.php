5f3e8db8ad5e46942ea4339361dd4c2e38d41cb8
Joel Unge <joel.unge@gmail.com>
2019-01-13 21:10:01 +0100

    indicators and candles

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,7 @@ Route::get('/hot', 'TradesController@hot')->name('hot');
 Route::get('/hot/{coin}', 'TradesController@hotSingle')->name('hot_single');
 Route::post('/trades/import/upload', 'TradesController@upload')->name('upload');
 Route::get('/trades/scrape', 'TradesController@scrape')->name('scrape_trades');
+Route::get('/trades/scrape_candles', 'TradesController@scrapeCandles')->name('scrape_candles');
 Route::get('/trades/bfxtrades', 'TradesController@showBfxTrades')->name('show_bfx_trades');
+Route::get('/make_indicators', 'TradesController@makeIndicators')->name('make_indicators');
 Route::get('news', 'NewsController@list')->name('news');
